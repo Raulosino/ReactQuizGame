3fca2c0333ad3b2334045ae434b6e628247ad821
Heriberto <hrhproducciones@gmail.com>
2021-04-12 22:14:57 +0200

    Heriberto's part(Music and Films)

--- quizgame/src/actions/index.js
+++ quizgame/src/actions/index.js
@@ -1,4 +1,6 @@
-import { getGeneralKnowledge } from '../api'
+import { getGeneralKnowledge, getFilms, getMusic } from '../api'
+
+
 
 export const getDataGeneral = () => {
     return (dispatch) => {
@@ -7,6 +9,50 @@ export const getDataGeneral = () => {
             payload: null
         })
         getGeneralKnowledge().then(data => {
+            dispatch({
+                type: 'SUCCESS',
+                payload: data
+            })
+            console.log(data)
+        }).catch(error => {
+            dispatch({
+                type: 'FAILED',
+                payload: error
+            })
+        })
+    }
+}
+
+
+export const getDataMusic = () => {
+    return (dispatch) => {
+        dispatch({
+            type: 'START',
+            payload: null
+        })
+        getMusic().then(data => {
+            dispatch({
+                type: 'SUCCESS',
+                payload: data
+            })
+            console.log(data)
+        }).catch(error => {
+            dispatch({
+                type: 'FAILED',
+                payload: error
+            })
+        })
+    }
+}
+
+
+export const getDataFilms = () => {
+    return (dispatch) => {
+        dispatch({
+            type: 'START',
+            payload: null
+        })
+        getFilms().then(data => {
             dispatch({
                 type: 'SUCCESS',
                 payload: data
--- quizgame/src/reducers/index.js
+++ quizgame/src/reducers/index.js
@@ -24,6 +24,59 @@ const generalKnowledgeReducer = (resultGeneralQuestions = generalQuestions, acti
 
 }
 
+
+let musicQuestions = {
+    data: []
+}
+
+
+const musicReducer = (resultmusicQuestions = musicQuestions, action) => {
+
+    switch (action.type) {
+        case 'START':
+            musicQuestions = { data: [], status: 'START' }
+            return { ...musicQuestions }
+        case 'SUCCESS':
+            musicQuestions = { data: action.payload, status: 'SUCCESS' }
+            return { ...musicQuestions }
+        case 'FAILED':
+            musicQuestions = { data: [], status: 'FAILED' }
+            return { ...musicQuestions }
+        default:
+            return resultmusicQuestions
+    }
+
+}
+
+
+let filmQuestions = {
+    data: []
+}
+
+
+const filmReducer = (resultfilmQuestions = filmQuestions, action) => {
+
+    switch (action.type) {
+        case 'START':
+            filmQuestions = { data: [], status: 'START' }
+            return { ...filmQuestions }
+        case 'SUCCESS':
+            filmQuestions = { data: action.payload, status: 'SUCCESS' }
+            return { ...filmQuestions }
+        case 'FAILED':
+            filmQuestions = { data: [], status: 'FAILED' }
+            return { ...filmQuestions }
+        default:
+            return resultfilmQuestions
+    }
+
+}
+
+
 export default combineReducers({
-    resultGeneralQuestions: generalKnowledgeReducer
+    resultGeneralQuestions: generalKnowledgeReducer,
+    resultGeneralQuestions: musicReducer,
+    resultfilmQuestions: filmReducer
 })
+
+
