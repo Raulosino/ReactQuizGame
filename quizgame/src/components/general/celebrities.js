790ebdfef3bf2fd67add736be4368505615f8faa
abdullahhaidar <abudehhaidar8@gmail.com>
2021-04-12 12:36:39 +0200

    small fix

---
+++ quizgame/src/components/general/celebrities.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const celebrities = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default celebrities
--- quizgame/src/pages/developer.js
+++ quizgame/src/pages/developer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-export const Developer = () => {
+const Developer = () => {
     return (
         <div>
             developer page
