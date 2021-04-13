554d0855266f82c9aeccac18c282e17b301ad872
Mariana Pantelic <mari27line@yahoo.com>
2021-04-13 11:22:51 +0200

    get answers in general knowledge

--- quizgame/src/components/general/general-knowledge.js
+++ quizgame/src/components/general/general-knowledge.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import {getAnswers} from '../getAnswers';
 
 const GeneralKnowledge = (props) => {
 
   console.log(props.data.data);
   console.log(props.data.status);
+  let index = 0;
 
+  let newAnswers = []
   
-    //<h1>{props.data.data[0].question}</h1>
-    switch (props.data.status){
-        case 'START':
-            return <h2>LOADING...</h2>
-        case 'FAILED':
-            return <h2>FAILED</h2>
-        case 'SUCCESS':
-            return (
-                <h1>{props.data.data[0].question}</h1>
-            )
-        default: return null
-    }
+  switch (props.data.status){
+      case 'START':
+          return <h2>LOADING...</h2>
+      case 'FAILED':
+          return <h2>FAILED</h2>
+      case 'SUCCESS':
+          return (
+            <>
+              <h1>{props.data.data[index].question}</h1>
+              {newAnswers = getAnswers(props.data.data[index].correct_answer, props.data.data[index].incorrect_answers).map((elem, idx) => 
+                  <li key={idx}>{elem}</li>
+              )}
+            </>
+          )
+      default: return null
+  }
 
 };
 
---
+++ quizgame/src/components/getAnswers.js
@@ -0,0 +1,13 @@
+export const getAnswers = (str, arr) => {
+  let newArr = [];
+  newArr.push(str, ...arr);
+
+  for (var i = newArr.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = newArr[i];
+    newArr[i] = newArr[j];
+    newArr[j] = temp;
+  }
+
+  return newArr;
+};
