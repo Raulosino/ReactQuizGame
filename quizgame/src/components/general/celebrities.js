17524185fa3c3d41cb31100acaa1bee1a800f49b
abdullahhaidar <abudehhaidar8@gmail.com>
2021-04-13 11:38:07 +0200

    fetch celeb and geo

--- quizgame/src/components/general/celebrities.js
+++ quizgame/src/components/general/celebrities.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux'
+import {getAnswers} from '../getAnswers';
 
-const Celebrities = () => {
-    return (
-        <div>
 
-        </div>
-    )
+const Celebrities = (props) => {
+    console.log(props.data.data);
+    console.log(props.data.status);
+    
+    let index = 0;
+  
+    let newAnswers = []
+    
+    switch (props.data.status){
+        case 'START':
+            return <h2>LOADING...</h2>
+        case 'FAILED':
+            return <h2>FAILED</h2>
+        case 'SUCCESS':
+            return (
+              <>
+                <h1>{props.data.data[index].question}</h1>
+                {newAnswers = getAnswers(props.data.data[index].correct_answer, props.data.data[index].incorrect_answers).map((elem, idx) => 
+                    <li key={idx}>{elem}</li>
+                )}
+              </>
+            )
+        default: return null
+    }
 }
 
-export default Celebrities
+
+const mapStateToProps = (state) => {
+    return ({
+        data: state.resultCelebrities
+    })
+}
+
+
+export default connect(mapStateToProps)(Celebrities)
