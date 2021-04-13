724df379a3c59f1c5a4d576d3011a97eca47b4cc
Mariana Pantelic <mari27line@yahoo.com>
2021-04-13 09:34:12 +0200

    got the h1 displayed general knowledge

--- quizgame/src/components/general/general-knowledge.js
+++ quizgame/src/components/general/general-knowledge.js
@@ -5,9 +5,20 @@ const GeneralKnowledge = (props) => {
   console.log(props.data.data);
   console.log(props.data.status);
 
-  return (
-    <h1>{props.data.data[0].question}</h1>
-  );
+  
+    //<h1>{props.data.data[0].question}</h1>
+    switch (props.data.status){
+        case 'START':
+            return <h2>LOADING...</h2>
+        case 'FAILED':
+            return <h2>FAILED</h2>
+        case 'SUCCESS':
+            return (
+                <h1>{props.data.data[0].question}</h1>
+            )
+        default: return null
+    }
+  
   
 };
 
