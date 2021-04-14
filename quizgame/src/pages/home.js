2e9c970cd3a63109ae1a425c4300ca5c269ede06
Mariana Pantelic <mari27line@yahoo.com>
2021-04-15 00:12:11 +0200

    styled home page

--- quizgame/src/pages/home.js
+++ quizgame/src/pages/home.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is the home page</h1>
+        <div class="homeBg">
+            
+            <div className="home"></div>
             <Link to = "/play"><button>Play</button></Link>
             <Link to = "/about"><button>About</button></Link>
         </div>
