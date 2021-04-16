dd977c54401aeef1ccdbb1695122ee0ad5ba8be4
Mariana Pantelic <mari27line@yahoo.com>
2021-04-16 15:07:15 +0200

    fixed the sound :)

--- quizgame/src/index.js
+++ quizgame/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { createStore, applyMiddleware } from 'redux';
-import thunk from "redux-thunk"
-import { Provider } from 'react-redux'
-import reducers from './reducers'
-
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
 
 import About from "./pages/about";
 import Developer from "./pages/developer";
@@ -15,26 +13,36 @@ import General from "./pages/general";
 import Home from "./pages/home";
 import Play from "./pages/play";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./main.css";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route path="/play"><Play /></Route>
-        <Route path="/about"><About /></Route>
-        <Route path='/developer'><Developer /></Route>
-        <Route path='/general'><General /></Route>
-
-
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/play">
+          <Play />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/developer">
+          <Developer />
+        </Route>
+        <Route path="/general">
+          <General />
+        </Route>
       </Switch>
     </Router>
-  )
+  );
 };
 
 ReactDOM.render(
   <Provider store={createStore(reducers, applyMiddleware(thunk))}>
     <App />
-  </Provider>, document.getElementById("root"));
+  </Provider>,
+  document.getElementById("root")
+);
