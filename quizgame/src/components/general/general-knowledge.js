70b9d9d318e0de8b277183a42102f6c44de3c1e1
abdullahhaidar <abudehhaidar8@gmail.com>
2021-04-17 18:10:16 +0200

    update geo and general knowledge

--- quizgame/src/components/general/general-knowledge.js
+++ quizgame/src/components/general/general-knowledge.js
@@ -21,32 +21,56 @@ const GeneralKnowledge = (props) => {
 
   let newAnswers = [];
 
+  const askFriend = () => {
+    props.data.data[index].incorrect_answers.map((elem) => {
+      document.getElementById(`${elem}`).style.display = "none";
+      document.getElementById(
+        `${props.data.data[index].correct_answer}`
+      ).style.background = "green";
+    });
+    props.dispatch(updateScore(-50));
+  };
+
+  const getHelp = () => {
+    const newArr = [];
+    props.data.data[index].incorrect_answers.map((elem) => {
+      newArr.push(elem);
+    });
+    document.getElementById(`${newArr[0]}`).style.display = "none";
+    document.getElementById(`${newArr[1]}`).style.display = "none";
+    props.dispatch(updateScore(-30));
+  };
+
   const checkAnswer = (e) => {
     let answer = e.currentTarget.id;
     console.log(answer);
     if (answer === props.data.data[index].correct_answer) {
-      document.getElementById(`${answer}`).style.backgroundColor = "green";
+      document.getElementById(`${answer}`).style.background = "green";
       props.data.data[index].incorrect_answers.map((elem) => {
-        document.getElementById(`${elem}`).style.backgroundColor = "red";
+        document.getElementById(`${elem}`).style.background = "red";
       });
       props.dispatch(updateScore(100));
       winningSound.play();
+      winningSound.volume = 0.1;
     } else {
       document.getElementById(
         `${props.data.data[index].correct_answer}`
-      ).style.backgroundColor = "green";
+      ).style.background = "green";
       props.data.data[index].incorrect_answers.map((elem) => {
-        document.getElementById(`${elem}`).style.backgroundColor = "red";
+        document.getElementById(`${elem}`).style.background = "red";
       });
       props.dispatch(updateScore(-10));
       wrongAnswerSound.play();
+      wrongAnswerSound.volume = 0.1;
     }
   };
 
   const goToNext = () => {
-    document
-      .querySelectorAll("button")
-      .forEach((elem) => (elem.style.backgroundColor = "rgb(228, 163, 41)"));
+    document.querySelectorAll("button").forEach((elem) => {
+      elem.style.background =
+        "repeating-linear-gradient( 45deg, #ffc800, #ffc800 5px, #ffc200 5px, #ffc200 10px)";
+      elem.style.display = "block";
+    });
     if (state.index === props.data.data.length - 1) {
       setState({ index: 0 });
     } else setState({ index: state.index + 1 });
@@ -59,7 +83,7 @@ const GeneralKnowledge = (props) => {
       return <h2>FAILED</h2>;
     case "SUCCESS":
       return (
-        <div className="genKnowledgeBg generalBg">
+        <div className="genKnowledge celebritiesBg generalBg">
           <Container className="mt-5">
             <div className="mainContainer">
               <div className="headingBox">
@@ -72,7 +96,7 @@ const GeneralKnowledge = (props) => {
               </div>
               <Row>
                 <Col lg={3}>
-                  <div className="guy guyGenKnowledge"></div>
+                  <div className="guy guyCelebrities"></div>
                 </Col>
                 <Col lg={7} className="genContainer">
                   {
@@ -85,7 +109,7 @@ const GeneralKnowledge = (props) => {
                         id={elem}
                         onClick={(e) => checkAnswer(e)}
                         block
-                        className="genBtn"
+                        className="game-button orange"
                       >
                         <span
                           className="text-center"
@@ -94,22 +118,23 @@ const GeneralKnowledge = (props) => {
                       </Button>
                     )))
                   }
-                  <div className=" d-flex justify-content-between mt-5 col-centered">
-                    <Link to="/general">
-                      <Button className="backBtn">Back</Button>
-                    </Link>
-                    <Button onClick={goToNext} className="nextBtn">
-                      Next
-                      {/*  <audio
-                        src="../../../sounds/sounds_fff_correct.mp3"
-                        id="winningSound"
-                      /> */}
-                    </Button>
-                  </div>
+                  <Button onClick={goToNext} className="" id="nextBtn">
+                    Next
+                  </Button>
                 </Col>
               </Row>
             </div>
           </Container>
+          <div onClick={askFriend} className="helpBtn" id="askFriend">
+            <i class="fas fa-user fa-2x"></i>
+          </div>
+          <div onClick={getHelp} className="helpBtn" id="help50">
+            {" "}
+            50/50
+          </div>
+          <Link to="/general">
+            <div className="backBtn">Categories</div>
+          </Link>
         </div>
       );
     default:
