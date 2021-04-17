7a1380f505ca2bfe390baa96c9985283b667903c
Mariana Pantelic <mari27line@yahoo.com>
2021-04-17 14:25:23 +0200

    changed sound volume

--- quizgame/src/components/general/celebrities.js
+++ quizgame/src/components/general/celebrities.js
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { getAnswers } from "../getAnswers";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import { updateScore } from '../../actions';
+import { updateScore } from "../../actions";
 import { Link } from "react-router-dom";
 import winning from "../../sounds/Game-show-winning.mp3";
 import wrong from "../../sounds/Wrong-answer-sound-effect.mp3";
 
-
 const Celebrities = (props) => {
-  const [state, setState] = useState({ index: 0 })
+  const [state, setState] = useState({ index: 0 });
 
   console.log(props.data.data);
   console.log(props.data.status);
@@ -24,90 +23,124 @@ const Celebrities = (props) => {
 
   const askFriend = () => {
     props.data.data[index].incorrect_answers.map((elem) => {
-      document.getElementById(`${elem}`).style.display = 'none'
-      document.getElementById(`${props.data.data[index].correct_answer}`).style.background = "green"
-    })
-    props.dispatch(updateScore(-50))
-  }
+      document.getElementById(`${elem}`).style.display = "none";
+      document.getElementById(
+        `${props.data.data[index].correct_answer}`
+      ).style.background = "green";
+    });
+    props.dispatch(updateScore(-50));
+  };
 
   const getHelp = () => {
-
     const newArr = [];
     props.data.data[index].incorrect_answers.map((elem) => {
-      newArr.push(elem)
-    })
-    document.getElementById(`${newArr[0]}`).style.display = 'none';
-    document.getElementById(`${newArr[1]}`).style.display = 'none'
-    props.dispatch(updateScore(-30))
-
-  }
+      newArr.push(elem);
+    });
+    document.getElementById(`${newArr[0]}`).style.display = "none";
+    document.getElementById(`${newArr[1]}`).style.display = "none";
+    props.dispatch(updateScore(-30));
+  };
 
   const checkAnswer = (e) => {
     let answer = e.currentTarget.id;
-    console.log(answer)
+    console.log(answer);
     if (answer === props.data.data[index].correct_answer) {
-      document.getElementById(`${answer}`).style.background = "green"
+      document.getElementById(`${answer}`).style.background = "green";
       props.data.data[index].incorrect_answers.map((elem) => {
-        document.getElementById(`${elem}`).style.background = 'red'
-      })
+        document.getElementById(`${elem}`).style.background = "red";
+      });
       props.dispatch(updateScore(100));
       winningSound.play();
+      winningSound.volume = 0.1;
     } else {
-      document.getElementById(`${props.data.data[index].correct_answer}`).style.background = "green"
+      document.getElementById(
+        `${props.data.data[index].correct_answer}`
+      ).style.background = "green";
       props.data.data[index].incorrect_answers.map((elem) => {
-        document.getElementById(`${elem}`).style.backgroundColor = 'red'
+        document.getElementById(`${elem}`).style.background = "red";
       });
       props.dispatch(updateScore(-10));
       wrongAnswerSound.play();
+      wrongAnswerSound.volume = 0.1;
     }
-  }
+  };
 
   const goToNext = () => {
-
-    document.querySelectorAll('button').forEach(elem => {
-      elem.style.background = 'repeating-linear-gradient( 45deg, #ffc800, #ffc800 5px, #ffc200 5px, #ffc200 10px)'
-      elem.style.display = 'block';
-    })
+    document.querySelectorAll("button").forEach((elem) => {
+      elem.style.background =
+        "repeating-linear-gradient( 45deg, #ffc800, #ffc800 5px, #ffc200 5px, #ffc200 10px)";
+      elem.style.display = "block";
+    });
     if (state.index === props.data.data.length - 1) {
-      setState({ index: 0 })
-    } else setState({ index: state.index + 1 })
-  }
+      setState({ index: 0 });
+    } else setState({ index: state.index + 1 });
+  };
 
   switch (props.data.status) {
-    case 'START':
-      return <h2>LOADING...</h2>
-    case 'FAILED':
-      return <h2>FAILED</h2>
-    case 'SUCCESS':
+    case "START":
+      return <h2>LOADING...</h2>;
+    case "FAILED":
+      return <h2>FAILED</h2>;
+    case "SUCCESS":
       return (
-        <div className='genKnowledge celebritiesBg generalBg'>
+        <div className="genKnowledge celebritiesBg generalBg">
           <Container className="mt-5">
             <div className="mainContainer">
               <div className="headingBox">
-                <h2 className="text-center" dangerouslySetInnerHTML={{ __html: props.data.data[index].question, }} />
+                <h2
+                  className="text-center"
+                  dangerouslySetInnerHTML={{
+                    __html: props.data.data[index].question,
+                  }}
+                />
               </div>
               <Row>
                 <Col lg={3}>
                   <div className="guy guyCelebrities"></div>
                 </Col>
                 <Col lg={7} className="genContainer">
-                  {newAnswers = getAnswers(props.data.data[index].correct_answer, props.data.data[index].incorrect_answers).map((elem, idx) =>
-                    <Button key={idx} id={elem} onClick={(e) => checkAnswer(e)} block className="game-button orange"><span className="text-center" dangerouslySetInnerHTML={{ __html: elem }} /></Button>
-                  )}
+                  {
+                    (newAnswers = getAnswers(
+                      props.data.data[index].correct_answer,
+                      props.data.data[index].incorrect_answers
+                    ).map((elem, idx) => (
+                      <Button
+                        key={idx}
+                        id={elem}
+                        onClick={(e) => checkAnswer(e)}
+                        block
+                        className="game-button orange"
+                      >
+                        <span
+                          className="text-center"
+                          dangerouslySetInnerHTML={{ __html: elem }}
+                        />
+                      </Button>
+                    )))
+                  }
                   <div className=" d-flex justify-content-between mt-5 col-centered">
-                    <Link to="/general"><Button className='game-button orange'>Back</Button></Link>
-                    <Button onClick={goToNext} className="game-button orange">Next</Button>
-                    <Button onClick={askFriend} className="game-button orange">Ask a Friend</Button>
-                    <Button onClick={getHelp} className="game-button orange"> 50/50</Button>
+                    <Link to="/general">
+                      <Button className="game-button orange">Back</Button>
+                    </Link>
+                    <Button onClick={goToNext} className="game-button orange">
+                      Next
+                    </Button>
+                    <Button onClick={askFriend} className="game-button orange">
+                      Ask a Friend
+                    </Button>
+                    <Button onClick={getHelp} className="game-button orange">
+                      {" "}
+                      50/50
+                    </Button>
                   </div>
                 </Col>
               </Row>
             </div>
           </Container>
-
         </div>
-      )
-    default: return null
+      );
+    default:
+      return null;
   }
 };
 
