f908321fd17b6efdd03f87b0aad4b6c613289836
Mariana Pantelic <mari27line@yahoo.com>
2021-04-18 17:43:09 +0200

    added coments

--- quizgame/src/components/general/general-knowledge.js
+++ quizgame/src/components/general/general-knowledge.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { getAnswers } from "../getAnswers";
@@ -9,6 +9,7 @@ import wrong from "../../sounds/Wrong-answer-sound-effect.mp3";
 import click from "../../sounds/Mouse_Click_1-fesliyanstudios.com.mp3";
 
 const GeneralKnowledge = (props) => {
+  //declaring the state for the index
   const [state, setState] = useState({ index: 0 });
 
   console.log(props.data.data);
@@ -16,6 +17,7 @@ const GeneralKnowledge = (props) => {
 
   let index = state.index;
 
+  //declaring sound variables
   let winningSound = new Audio(winning);
 
   let wrongAnswerSound = new Audio(wrong);
@@ -29,6 +31,7 @@ const GeneralKnowledge = (props) => {
 
   let newAnswers = [];
 
+  //function for ask a friend option (showing only the correct answer)
   const askFriend = () => {
     audioPlay();
     props.data.data[index].incorrect_answers.map((elem) => {
@@ -40,6 +43,7 @@ const GeneralKnowledge = (props) => {
     props.dispatch(updateScore(-50));
   };
 
+  //function for 50/50 option (removes 2 incorrect answers)
   const getHelp = () => {
     audioPlay();
     const newArr = [];
@@ -51,6 +55,7 @@ const GeneralKnowledge = (props) => {
     props.dispatch(updateScore(-30));
   };
 
+  //function to check if the clicked answer is correct, changing the style and adding sounds
   const checkAnswer = (e) => {
     let answer = e.currentTarget.id;
     console.log(answer);
@@ -75,6 +80,7 @@ const GeneralKnowledge = (props) => {
     }
   };
 
+  //function to go to the next question when the button is clicked
   const goToNext = () => {
     audioPlay();
     document.querySelectorAll("button").forEach((elem) => {
@@ -87,6 +93,7 @@ const GeneralKnowledge = (props) => {
     } else setState({ index: state.index + 1 });
   };
 
+  //switch case for rendering data
   switch (props.data.status) {
     case "START":
       return <h2>LOADING...</h2>;
@@ -111,6 +118,7 @@ const GeneralKnowledge = (props) => {
                 </Col>
                 <Col lg={7} className="genContainer">
                   {
+                    //map through the array with both correct and incorrect answers and display them randomly
                     (newAnswers = getAnswers(
                       props.data.data[index].correct_answer,
                       props.data.data[index].incorrect_answers
@@ -118,6 +126,7 @@ const GeneralKnowledge = (props) => {
                       <button
                         key={idx}
                         id={elem}
+                        //onclick function to check if the clicked answer is correct
                         onClick={(e) => checkAnswer(e)}
                         block
                         className="game-button orange outlineBtn"
@@ -129,6 +138,7 @@ const GeneralKnowledge = (props) => {
                       </button>
                     )))
                   }
+                  {/* button to go to the next question */}
                   <Button onClick={goToNext} className="" id="nextBtn">
                     Next
                   </Button>
@@ -136,14 +146,17 @@ const GeneralKnowledge = (props) => {
               </Row>
             </div>
           </Container>
+          {/*ask a friend option */}
           <div onClick={askFriend} className="helpBtn" id="askFriend">
             <i class="fas fa-user fa-2x"></i>
           </div>
+          {/*50/50 option */}
           <div onClick={getHelp} className="helpBtn" id="help50">
             {" "}
             50/50
           </div>
           <Link to="/general">
+            {/*back to categories page */}
             <div className="backBtn" onClick={audioPlay}>
               Categories
             </div>
@@ -155,10 +168,12 @@ const GeneralKnowledge = (props) => {
   }
 };
 
+//using mapStateToProps to get the data from the Redux store
 const mapStateToProps = (state) => {
   return {
     data: state.resultGeneralQuestions,
   };
 };
 
+//connect the component with the Redux store
 export default connect(mapStateToProps)(GeneralKnowledge);
